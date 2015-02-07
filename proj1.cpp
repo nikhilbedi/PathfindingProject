ae06ed0980dde1a5abe8e8f63095ea1d8e69a18b
Nikhil Bedi <nbedi@usc.edu>
2015-02-07 15:35:11 -0800

    Question 3 finished. Added a constant to check if there was a closer goal near pacman at any point.

--- proj1.cpp
+++ proj1.cpp
@@ -153,6 +153,101 @@ vector<string> aStar(pair<int,int>& current, pair<int,int>& goal, Problem& probl
 	return path;
 }
 
+vector<string> aStarTricky(pair<int,int>& current, pair<int,int>& goal, Problem& problem, vector< pair<int,int> >& goals)
+{
+	vector<string> path;
+	set< pair<int,int> > closedSet;
+	set< pair<int,int> > openSet;
+	map< pair<int,int>, pair<int,int> > parents; 
+	map< pair<int,int>, int> gValues; 
+
+	// Add the first node to the closed set
+	// The closed set contains nodes that have already been evaluated
+	closedSet.insert(current);
+	gValues.insert(make_pair(current, 0));
+	do 
+	{
+		// For each node adjacent to current, evaluate it
+		vector<pair<int, int> > neighbors = problem.getSuccessors(current);
+
+		// A Star tricky addition. For each neighbor, check if it is a goal that is NOT the goal we're looking for
+		for(int i =0; i < neighbors.size(); i++)
+		{
+			pair<int,int> node = neighbors[i];
+			if (node.first != goal.first || node.second != goal.second)
+			{
+				for(int j = 0; j < goals.size(); j++)
+				{
+					if(goals[j].first == node.first && goals[j].second == node.second)
+					{
+						// Reassign goal?
+						goal = goals[j];
+						goals.erase(goals.begin() + j);
+						break;
+					}
+				}
+			}
+		}
+
+		for(int i=0; i<neighbors.size(); ++i)
+		{
+			pair<int,int> n = neighbors[i];
+			// If this node is in the closedSet, simply continue to the next neighbor
+			if(closedSet.find(n) != closedSet.end())
+			{
+				continue;
+			}
+
+			// If this node is in the openSet, check if we should reassign the parent
+			set< pair<int,int> >::iterator it = openSet.find(n);
+			if (it != openSet.end())	// check if this searches by value, not reference (otherwise, modify comparison operator)
+			{
+				// Obtain current g value
+				map< pair<int,int>, int >::iterator currentNode = gValues.find(current);
+				int current_g = currentNode->second;
+				int new_g = current_g + 1;
+
+				// Obtain n's g value (if the n is in the openSet, it is strictly assumed it has been assigned a g-value)
+				map< pair<int,int>, int >::iterator nNode = gValues.find(n);
+				int old_g = nNode->second;
+
+				// if current is a better parent, replace
+				if(new_g < old_g)	
+				{
+					nNode->second = new_g;
+				}
+			}
+			// else, compute heuristic and add to open set
+			else 
+			{
+				parents.insert(make_pair(n, current)); 	// n's parent is the current node
+				//n->h = computeManhattanDistance(n, goal);
+				// Obtain current G value, and use that to compute n's g value
+				map< pair<int,int>, int >::iterator currentNode = gValues.find(current);
+				int current_g = currentNode->second;
+				gValues.insert(make_pair(n, current_g + 1));
+				openSet.insert(n);
+			}
+		}
+
+		// if the open set is empty, we are done
+		if(openSet.empty())
+			break;
+
+		// Grab the node with smallest f value off of openset, and move current to the closed set
+		pair <int, int> pastState = current;
+		current = obtainSmallestCostNode(gValues, openSet, goal);
+		closedSet.insert(current);
+		openSet.erase(current);
+	}
+	while(current.first != goal.first || current.second != goal.second);
+
+	// Remove first element, because for some reason it's garbage :P
+	path = getPath(parents, goal);
+	path.erase(path.begin());
+	return path;
+}
+
 vector<string> questionOne(Problem &problem)
 {
 	pair<int,int> current = problem.getStartState();
@@ -213,8 +308,45 @@ vector<string> questionTwo(Problem &problem)
 
 vector<string> questionThree(Problem &problem)
 {
-	/// write your own code here
-	return vector<string>();
+	vector<string> mergedPath;
+
+	pair<int,int> current = problem.getStartState();
+	vector<pair<int, int> > goals = problem.getGoals();
+
+	// If there is no goal, return empty
+	if (goals.size() <= 0)
+		return vector<string>();
+
+	
+	// While there is still a goal
+	//int i = 0;
+	while(!goals.empty())
+	{
+		// Assign goal to be closest Manhattan distance from the current state
+		pair<int,int> goal;
+		int min = 1000000;
+		int indexToRemove = 0;
+		for(int i = 0; i < goals.size(); i++)
+		{
+			int temp = computeManhattanDistance(current, goals[i]);
+			if(temp < min)
+			{
+				min = temp;
+				goal = goals[i];
+				indexToRemove = i;
+			}
+		}
+
+		// Obtain path and add to mergedPath
+		pair<int,int> attemptGoal = goal;	// TODO remove
+		vector<string> path = aStarTricky(current, goal, problem, goals);
+		mergedPath.insert(mergedPath.end(), path.begin(), path.end());
+
+		// remove the chosen goal
+		goals.erase(goals.begin() + indexToRemove);
+	}
+
+	return mergedPath;
 }
 
 vector<string> questionFour(Problem &problem)
